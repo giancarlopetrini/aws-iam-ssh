03cb6754cdd5da09eefe73bd05df24fec1d7e851
Giancarlo Petrini <giancarlo@libreworx.com>
2017-12-19 15:36:06 -0500

    removed unneeded IAM key pull

--- linux-files/new-import-iam-ssh-keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-#grab key files
-aws iam list-ssh-public-keys --user-name "giancarlo" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read -r KeyId; \
-do
-  aws iam get-ssh-public-key --user-name "giancarlo" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text \
-   >> test.txt
-done
